bc28ad20c36e99126c96fda598fb2b809aa41fa5
Ian-Moffett <hscorp99906@gmail.com>
2022-03-18 17:15:37 -0400

    Added section enum.

--- src/include/CodeGen.h
+++ src/include/CodeGen.h
@@ -4,6 +4,8 @@
 #include <include/AST.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 
 /*
@@ -11,6 +13,13 @@
  *  and generate code based on
  *  what it finds.
  */
+
+typedef enum {
+    S_TEXT,
+    S_BSS,
+    S_RODATA,
+    S_DATA
+} elf_section_t;
 
 
 void compile(ast_t* ast);
