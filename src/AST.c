ad9d793b8475b2644c7c6b9d436513e84bd0526c
Ian-Moffett <hscorp99906@gmail.com>
2022-03-18 14:55:23 -0400

    Implemented.

--- src/AST.c
+++ src/AST.c
@@ -1,5 +1,35 @@
 #include <include/AST.h>
 
+void ast_init(ast_t* ast) {
+    ast->nodes = malloc(sizeof(ast_node_t));
+    ast->idx = 0;
+}
+
+
+void ast_push_node(ast_t* ast, ast_node_t node) {
+    ast->nodes[ast->idx++] = node;
+    ast->nodes = realloc(ast->nodes, sizeof(ast_node_t) * (ast->idx + 1));
+}
+
+
+ast_node_t ast_create_node(char* typelabel, char* token) {
+    ast_node_t node = {
+        .type = typelabel, 
+    };
+
+   node.value.str = token;
+   return node;
+}
+
+
+ast_node_t ast_create_nodeint(char* typelabel, uint64_t dec) {
+    ast_node_t node = {
+        .type = typelabel
+    };
+
+    node.value.dec = dec;
+}
+
 
 void ast_destroy(ast_t ast) {
     free(ast.nodes);
