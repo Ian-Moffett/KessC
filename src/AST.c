009581eee457aa6269a505c79937b74d9879acf8
Ian-Moffett <hscorp99906@gmail.com>
2022-03-18 15:19:18 -0400

    Added children.

--- src/AST.c
+++ src/AST.c
@@ -24,13 +24,31 @@ ast_node_t ast_create_node(char* typelabel, char* token) {
 
 ast_node_t ast_create_nodeint(char* typelabel, uint64_t dec) {
     ast_node_t node = {
-        .type = typelabel
+        .type = typelabel,
+        .children = NULL,
+        .childrenIdx = 0
     };
 
     node.value.dec = dec;
 }
 
+void node_init_children(struct AstNode** childrenPtr) {
+    *childrenPtr = malloc(sizeof(struct AstNode));
+}
+
+
+void node_push_child(ast_node_t* dest, ast_node_t src) {
+    dest->children[dest->childrenIdx++] = src;
+    dest->children = realloc(dest->children, sizeof(struct AstNode) * (dest->childrenIdx + 1));
+}
+
 
 void ast_destroy(ast_t ast) {
+    for (int i = 0; i < ast.idx + 1; ++i) {
+        if (ast.nodes[i].children) {
+            free(ast.nodes[i].children);
+        }
+    }
+
     free(ast.nodes);
 }
--- src/include/AST.h
+++ src/include/AST.h
@@ -6,8 +6,10 @@
 #include <stdlib.h>
 
 
-typedef struct {
+typedef struct AstNode {
     const char* type;
+    struct AstNode* children;
+    unsigned int childrenIdx;
 
     union {
         char* str;
@@ -26,6 +28,8 @@ void ast_init(ast_t* ast);
 void ast_push_node(ast_t* ast, ast_node_t node);
 ast_node_t ast_create_node(char* typelabel, char* str);
 ast_node_t ast_create_nodeint(char* typelabel, uint64_t dec);
+void node_init_children(struct AstNode** childrenPtr);
+void node_push_child(ast_node_t* dest, ast_node_t src);
 void ast_destroy(ast_t ast);
 
 #endif
