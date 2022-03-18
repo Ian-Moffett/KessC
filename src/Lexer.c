13ff3ff065adc4887f037d7882f23e6396df36cb
Ian-Moffett <hscorp99906@gmail.com>
2022-03-18 07:08:53 -0400

    Update lbuf idx.

--- src/Lexer.c
+++ src/Lexer.c
@@ -1,7 +1,6 @@
 // Written by Ian-Moffett.
 
 #include <include/Lexer.h>
-#include <string.h>
 
 void lexer_init(lexer_t* lexer, char* buffer) {
     lexer->bufsize = strlen(buffer);
@@ -15,13 +14,12 @@ void tokenize(lexer_t* lexer) {
     char* lexbuf = calloc(2, sizeof(char));
     unsigned long lbufidx = 0;
 
-    while (lexer->idx++ < lexer->bufsize) {
-        lexer->curChar = lexer->buffer[lexer->idx];
+    while (lexer->idx < lexer->bufsize) {
+        lexer->curChar = lexer->buffer[lexer->idx];     // Set the current char.
+        ++lbufidx;                                      // Increment index after done using current char.
 
-        ++lbufidx;
-
-        lexbuf = realloc(lexbuf, sizeof(char) * lbufidx + 1);
-        ++lexer->idx;
+        lexbuf = realloc(lexbuf, sizeof(char) * lbufidx + 1);       // Realloc the buffer somewhere else in memory.
+        ++lexer->idx;                                               // Increment buffer index.
     }
 
     free(lexbuf);
